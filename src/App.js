a588f68f28ba75939c7197565cdcc64bcb92c586
Devesh Mukherjee <deveshmukherjee76@gmail.com>
2023-01-16 03:15:23 +0530

    carousel completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  Header from "./components/header/Header";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route index element={<Home></Home>}></Route>
+          <Route path="movie/:id" element={<h1>Movie detail page</h1>}></Route>
+          <Route path="movies/:type" element={<h1>Movie list page</h1>}></Route>
+          <Route path="/*" element={<h1>Error 404 :)</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }

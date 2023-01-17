6caca91ff87afb76d261d2b77e57b0b140984e46
Devesh Mukherjee <deveshmukherjee76@gmail.com>
2023-01-17 17:07:57 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import  Header from "./components/header/Header";
-import Home from "./pages/home/Home";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import MovieList from './components/movieList/movieList';
+import Movie from './pages/movieDetail/movie';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header></Header>
-        <Routes>
-          <Route index element={<Home></Home>}></Route>
-          <Route path="movie/:id" element={<h1>Movie detail page</h1>}></Route>
-          <Route path="movies/:type" element={<h1>Movie list page</h1>}></Route>
-          <Route path="/*" element={<h1>Error 404 :)</h1>}></Route>
-        </Routes>
-      </Router>
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<Home />}></Route>
+                <Route path="movie/:id" element={<Movie />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
     </div>
   );
 }
---
+++ src/components/movieList/movieList.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from "react"
+import "./movieList.css"
+import { useParams } from "react-router-dom"
+import Cards from "../card/card"
+
+const MovieList = () => {
+    
+    const [movieList, setMovieList] = useState([])
+    const {type} = useParams()
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    useEffect(() => {
+        getData()
+    }, [type])
+
+    const getData = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=6cd36314fe23ed6e568307d16cd6ec66&language=en-US`)
+        .then(res => res.json())
+        .then(data => setMovieList(data.results))
+    }
+
+    return (
+        <div className="movie__list">
+            {/* ("top rated" ? (type == "top_rated") : type) */}
+            <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+            <div className="list__cards">
+                {
+                    movieList.map(movie => (
+                        <Cards movie={movie} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MovieList
